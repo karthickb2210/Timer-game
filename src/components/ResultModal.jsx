e2a414742d1f15d22f4998a457e05b0d77e98aef
karthickb2210 <116095490+karthickb2210@users.noreply.github.com>
2024-01-04 14:16:14 +0530

    Add files via upload

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,15 +1,21 @@
-import { forwardRef  } from "react";
-const Resultmodal = forwardRef(function ResultModal({results,targetTime},ref){
+import { useRef ,forwardRef } from "react";
+const Resultmodal = forwardRef(function ResultModal({onReset,targetTime,timeRem},ref){
+  const dialog = useRef();
+  const userLost = timeRem <=0;
+  const formattedTime = (timeRem/1000).toFixed(2);
+  const score = Math.round((1-timeRem/(targetTime*1000))*100)
+
     return(
         <dialog ref={ref} className="result-modal">
-            <h2>You {results}</h2>
+            {userLost && <h2>You Lost</h2>}
+            {!userLost && <h2>Your Score :{score}</h2> }
             <p>
                 The target time was <strong>{targetTime} seconds</strong>
             </p>
             <p>
-                Ypu stopped the timer with <strong>X seconds left </strong>
+                Ypu stopped the timer with <strong>{formattedTime}seconds left </strong>
             </p>
-            <form method="dialog">
+            <form method="dialog" onSubmit={onReset}>
                 <button>Close</button>
             </form>
         </dialog>
--- src/components/Timerchallenge.jsx
+++ src/components/Timerchallenge.jsx
@@ -1,37 +1,38 @@
 import { useRef, useState } from "react";
 import Resultmodal from "./ResultModal";
 export default function Timerchallenge({title,targetTime}){
-    // const [timerExpired,setTimerExpired] = useState(false);
-    // const [timerstarted,setTimerstarted] = useState(false);
     const [timeLeft,setTimeLeft] = useState(targetTime*1000);
     const timer = useRef();
     const dialog = useRef();
     const timerIsActive = timeLeft >0 && timeLeft <targetTime*1000;
     if(timeLeft <=0){
-        clearInterval(timer.current);
+        clearInterval(timer.current);      
+        dialog.current.showModal();
+    }
+    function handleReset(){
         setTimeLeft(targetTime*1000);
-        dialog.current.open();
     }
     function handleStart(){
         timer.current = setInterval(()=>{
             setTimeLeft(prevtime => prevtime-10)
-        },10)
+        },10);
     }
     function handleStop(){
-        dialog.current.open();
+        dialog.current.showModal();
         clearInterval(timer.current);
     }
     
     return (
         <>
-        <Resultmodal ref={dialog} targetTime={targetTime} results="loss"/>
+        <Resultmodal  onReset={handleReset}
+         ref={dialog} targetTime={targetTime} timeRem={timeLeft}/>
         <section className="challenge">
     <h2>{title}</h2>
     <p className="challenge-time">
         {targetTime} second {targetTime >1 ? 's': ''}
     </p>
     <p> 
-        <button onClick={ !timerIsActive ? handleStart:handleStop}>{ !timerstarted ? 'Start':'stop'} challenge</button>
+        <button onClick={ !timerIsActive ? handleStart:handleStop}>{ !timerIsActive? 'Start':'stop'} challenge</button>
     </p>
     <p className={timerIsActive ? "active" :undefined}> 
     {timerIsActive ? 'timer running':'Timer inactive'}
